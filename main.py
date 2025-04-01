daa52256068acab3a011e1c0138f3f477b3b686a
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2025-04-01 15:23:01 +0000

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from cosmosdb-python-101!")
+import os 
+from azure.identity import DefaultAzureCredential
+from azure.cosmos import CosmosClient
+from dotenv import load_dotenv
 
+load_dotenv()
+ENDPOINT = os.getenv('COSMOS_ENDPOINT')
 
-if __name__ == "__main__":
-    main()
+credential = DefaultAzureCredential()
+
+try:
+    client = CosmosClient(ENDPOINT, credential)
+    for db in client.list_databases():
+        print(f'Database name: {db['id']}')
+except Exception as e:
+    print(f'There was an error: {e}')
